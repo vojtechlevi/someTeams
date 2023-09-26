c9aefa9d66193cff37656488063b33152b2a6e51
Levi Ekström <vojtechlevi1@hotmail.com>
2023-09-26 14:27:23 +0200

    first commit

---
+++ data.php
@@ -0,0 +1,47 @@
+<?php
+
+$teams = [
+    'Arsenal' => [
+        'league' => 'Premier league',
+        'last-time-champions' => 2004,
+        'city' => 'London',
+        'url' => 'https://www.arsenal.com/'
+    ],
+    'Aston Villa' => [
+        'league' => 'Premier league',
+        'last-time-champions' => 1981,
+        'city' => 'Birmingham',
+        'url' => 'https://www.avfc.co.uk/'
+    ],
+    'Manchester City' => [
+        'league' => 'Premier league',
+        'last-time-champions' => 2023,
+        'city' => 'Manchester',
+        'url' => 'https://www.mancity.com/'
+    ],
+    'Newcastle' => [
+        'league' => 'Premier league',
+        'last-time-champions' => 1927,
+        'city' => 'Newcastle',
+        'nickname' => 'Magpies',
+        'url' => 'https://www.nufc.co.uk/'
+    ],
+    'Chelsea' => [
+        'league' => 'Premier league',
+        'last-time-champions' => 2017,
+        'city' => 'London',
+        'url' => 'https://www.chelseafc.com/en'
+    ],
+    'Watford' => [
+        'league' => 'Championship',
+        'last-time-champions' => null,
+        'city' => 'London',
+        'url' => 'https://www.watfordfc.com/'
+    ],
+    'Brighton' => [
+        'league' => 'Premier league',
+        'last-time-champions' => null,
+        'city' => 'Brighton',
+        'url' => 'https://www.brightonandhovealbion.com/'
+    ],
+];
